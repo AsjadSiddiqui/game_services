c730b4fc2a5176a38ce1b744fc44811ad787fe03
AsjadSiddiqui <asjadsidd@outlook.com>
2021-07-18 15:40:27 +0500

    Latest

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -51,7 +51,7 @@ class GamesServices {
   /// To sign in the user.
   /// You need to call the sign in before making any action,
   /// (like sending a score or unlocking an achievement).
-  static Future<String?> signIn() async {
+  static Future<dynamic> signIn() async {
     return await GamesServicesPlatform.instance.signIn();
   }
 
